19b7729d3f314bd714ead1d7ea4d5933bed9c7f9
Edward Wible <aewible@gmail.com>
2012-11-12 17:36:12 -0500

    another to_s error bug fix

--- lib/ruby_omx/response/response_error.rb
+++ lib/ruby_omx/response/response_error.rb
@@ -11,7 +11,8 @@ module RubyOmx
     xml_reader :error_messages, :as=>[ErrorMessage], :in=>'ErrorData'
     
     def to_s
-      self.errors.collect{ |e| e.message.to_s }.join(',')
+      error_string = self.errors ? self.errors.collect{ |e| e.message.to_s }.join(',') : "none"
+      return "Errors: #{error_string}"
     end
 
   end
